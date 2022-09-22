760728a8cc5bc6f7cb733d13e08d11cee8c4190a
mufidmoesz <mufidmusyaffa@365.telkomuniversity.ac.id>
2022-09-22 12:20:04 +0700

    assets

--- lib/latihan/latihan_assets.dart
+++ lib/latihan/latihan_assets.dart
@@ -17,20 +17,84 @@ class _LatihanAssetsState extends State<LatihanAssets> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(children: [
-      ClipRRect(
-          borderRadius: const BorderRadius.only(
-              bottomLeft: Radius.circular(30),
-              bottomRight: Radius.circular(30)),
-          // child: const Text('hi mufid'),
-          child:
-              // const Text('hi mufid'),
-              Image.asset(
-            'assets/images/mountain.jpg',
-            width: double.infinity,
+        backgroundColor: Colors.grey,
+        body: Stack(children: [
+          Container(
             height: 300,
-            fit: BoxFit.cover,
-          )),
-    ]));
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              ),
+              image: DecorationImage(
+                image: AssetImage('assets/images/mountain.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                SizedBox(width: 20),
+                Text('Hi Mufid',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold)),
+                Spacer(),
+                SizedBox(
+                    height: 75,
+                    width: 75,
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage('assets/images/mufid.png'),
+                    )),
+                SizedBox(width: 20),
+              ],
+            ),
+          ),
+          Container(
+              margin: const EdgeInsets.fromLTRB(50, 200, 50, 100),
+              width: 500,
+              height: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                color: Colors.white,
+              ),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    const Text('Halo Button',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    const Text('Pencet Saya',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    SizedBox(
+                        width: 250,
+                        height: 50,
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              foregroundColor: Colors.black87,
+                              backgroundColor: Colors.amber,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                            ),
+                            onPressed: () {},
+                            child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: const [
+                                  Icon(Icons.abc),
+                                  Text('Pesan Saya Sekarang',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      )),
+                                ])))
+                  ])),
+        ]));
   }
 }
